4eb90255a1a6645aece613aa44ee6293f81fa8d3
Ziomioslaw <dawid.plocki@gmail.com>
2023-06-29 21:33:05 +0200

    Move the NoteTitle pipeline code

--- src/app/note-title.pipe.spec.ts
+++ src/app/note-title.pipe.spec.ts
@@ -1,8 +1,10 @@
 import { NoteTitlePipe } from './note-title.pipe';
 
 describe('NoteTitlePipe', () => {
+
   it('create an instance', () => {
     const pipe = new NoteTitlePipe();
     expect(pipe).toBeTruthy();
   });
+
 });
--- src/app/note-title.pipe.ts
+++ src/app/note-title.pipe.ts
@@ -1,12 +1,27 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Note } from './note';
 
 @Pipe({
   name: 'noteTitle'
 })
 export class NoteTitlePipe implements PipeTransform {
+  private static readonly TITLE_MAX_LENGTH = 20;
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: Note): unknown {
+    if (!value || !value.text) {
+        return '';
+    }
+
+    const title = value.text
+        .replace('\n', '')
+        .trim();
+
+    if (title == '') {
+        return '';
+    }
+
+    return title.length > NoteTitlePipe.TITLE_MAX_LENGTH
+        ? title.substr(0, NoteTitlePipe.TITLE_MAX_LENGTH) + '...'
+        : title;
   }
-
 }
