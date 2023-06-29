b230cb0d2d26ec13f457e324fe37526a70d9b3ff
Ziomioslaw <dawid.plocki@gmail.com>
2023-06-29 21:51:49 +0200

    Move LocalStorage and LoggerService code

--- old/app/services/logger.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class LoggerService {
-    public log(...params: any[]) {
-        console.log.apply(this, params);
-    }
-}
--- src/app/local-storage.service.ts
+++ src/app/local-storage.service.ts
@@ -1,22 +1,81 @@
 import { Injectable } from '@angular/core';
 import { Note } from './note';
+import { LoggerService } from './logger.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocalStorageService {
-  loadNotes(): Promise<Note[]> {
-    throw new Error('Method not implemented.');
-  }
-  saveNotes(notes: import("./note").Note[]): Promise<import("./note").Note[]> {
-    throw new Error('Method not implemented.');
-  }
-  saveNote(note: Note): Promise<import("./note").Note> {
-    throw new Error('Method not implemented.');
-  }
-  deleteNote(note: Note): Promise<import("./note").Note> {
-    throw new Error('Method not implemented.');
+  private static readonly LOCAL_STORAGE_KEY = "notes";
+
+  constructor(
+    private loggerService: LoggerService
+  ) {
   }
 
-  constructor() { }
+  public saveNotes(notes: Note[]): Promise<Note[]> {
+    return new Promise<Note[]>((resolve, reject) => {
+      this.loggerService.log('Save notes: ', notes);
+
+      this.saveRawDataIntoLocalStorage(notes);
+      resolve(notes);
+    });
+  }
+
+  public saveNote(note: Note): Promise<Note> {
+    return new Promise<Note>((resolve, reject) => {
+      this.loggerService.log('Save note: ', note);
+
+      const rawData = this.loadRawDataFromLocalStorage();
+      const rawNote = rawData.find((x: any) => x.id == note.id);
+
+      if (rawNote) {
+        rawNote.text = note.text;
+      } else {
+        rawData.push(note);
+      }
+
+      this.saveRawDataIntoLocalStorage(rawData);
+
+      resolve(note);
+    });
+  }
+
+  public deleteNote(note: Note): Promise<Note> {
+    return new Promise<Note>((resolve, reject) => {
+      this.loggerService.log('Delete note: ', note);
+
+      let allNotes = this.loadRawDataFromLocalStorage();
+      let noteWithoutOne = allNotes.filter((x: any) => x.id != note.id);
+
+      this.saveRawDataIntoLocalStorage(noteWithoutOne);
+
+      resolve(note);
+    });
+  }
+
+  public loadNotes(): Promise<Note[]> {
+    return new Promise<Note[]>((resolve, reject) => {
+      this.loggerService.log('Load notes');
+
+      resolve(this.loadRawDataFromLocalStorage()
+        .map((r: any) => new Note(r.text, r.id))
+      );
+    });
+  }
+
+  private loadRawDataFromLocalStorage(): any[] {
+    const rawData = localStorage.getItem(LocalStorageService.LOCAL_STORAGE_KEY)
+    if (!rawData) {
+      return [];
+    }
+
+    return JSON.parse(rawData);
+  }
+
+  private saveRawDataIntoLocalStorage(rawNotes: any[]) {
+    let rawData = JSON.stringify(rawNotes);
+
+    localStorage.setItem(LocalStorageService.LOCAL_STORAGE_KEY, rawData);
+  }
 }
