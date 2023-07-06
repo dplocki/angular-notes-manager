1247b05c4044aee3e3eea9b7b8538baac8e77e74
Ziomioslaw <dawid.plocki@gmail.com>
2023-07-06 21:48:24 +0200

    Remove unused test for AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,11 +2,14 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { NoteDetailComponent } from './note-detail/note-detail.component';
 import { NoteListComponent } from './note-list/note-list.component';
+import { FormsModule } from '@angular/forms';
+import { NoteTitlePipe } from './note-title.pipe';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AppComponent, NoteDetailComponent, NoteListComponent]
+      declarations: [AppComponent, NoteDetailComponent, NoteListComponent, NoteTitlePipe],
+      imports: [FormsModule]
     }).compileComponents();
   });
 
@@ -14,12 +17,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-notes-manager app is running!');
   });
 });
