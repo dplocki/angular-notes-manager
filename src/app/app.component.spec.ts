d7acaec9bdc72bef59fb8fd67b6d7a928fe48552
Ziomioslaw <dawid.plocki@gmail.com>
2023-07-06 21:05:59 +0200

    Fix unit test for AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { NoteDetailComponent } from './note-detail/note-detail.component';
+import { NoteListComponent } from './note-list/note-list.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent, NoteDetailComponent, NoteListComponent]
+    }).compileComponents();
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
