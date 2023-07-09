3b9656da1dbc23a3f9b09bbb2935728ffe52afee
Ziomioslaw <dawid.plocki@gmail.com>
2023-07-09 14:53:10 +0200

    Move fiels nextId into private

--- src/app/id-generator.service.ts
+++ src/app/id-generator.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class IdGeneratorService {
-  nextId = 1;
+  private nextId = 1;
 
   getIdForNew() {
     const id = this.nextId;
