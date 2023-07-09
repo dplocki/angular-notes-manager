99d194cfff89870f88e1fd595e5a3a9d64aceeb7
Ziomioslaw <dawid.plocki@gmail.com>
2023-07-09 14:52:42 +0200

    Add unit test for NoteService

--- src/app/note.service.spec.ts
+++ src/app/note.service.spec.ts
@@ -1,16 +1,41 @@
 import { TestBed } from '@angular/core/testing';
 
 import { NoteService } from './note.service';
+import { LocalStorageService } from './local-storage.service';
+import { IdGeneratorService } from './id-generator.service';
+import { Note } from './note';
 
 describe('NoteService', () => {
-  let service: NoteService;
+  let noteService: NoteService;
+  let storageServiceSpy: jasmine.SpyObj<LocalStorageService>;
+  let idGeneratorServiceSpy: jasmine.SpyObj<IdGeneratorService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(NoteService);
+    TestBed.configureTestingModule({
+      providers: [
+        NoteService,
+        { provide: LocalStorageService, useValue: jasmine.createSpyObj('LocalStorageService', ['loadNotes']) },
+        { provide: IdGeneratorService, useValue: jasmine.createSpyObj('IdGeneratorService', ['checkNumber']) }
+      ]
+    });
+
+    noteService = TestBed.inject(NoteService);
+    storageServiceSpy = TestBed.inject(LocalStorageService) as jasmine.SpyObj<LocalStorageService>;
+    idGeneratorServiceSpy = TestBed.inject(IdGeneratorService) as jasmine.SpyObj<IdGeneratorService>;
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('getNotes should return stored notes and checked their ids', async () => {
+    const loadNotes = [
+      new Note('a', 1),
+      new Note('b', 2),
+    ];
+
+    storageServiceSpy.loadNotes.and.resolveTo(loadNotes);
+    idGeneratorServiceSpy.checkNumber.and.returnValues();
+
+    const notes = await noteService.getNotes();
+
+    expect(notes).toEqual(loadNotes);
   });
+
 });
