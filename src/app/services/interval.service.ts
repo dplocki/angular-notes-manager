af4c400aa11e94c6fd92c94f341aef0734673d8a
Ziomioslaw <dawid.plocki@gmail.com>
2023-07-09 23:01:35 +0200

    Adjust the IntervalService

--- src/app/services/interval.service.ts
+++ src/app/services/interval.service.ts
@@ -4,13 +4,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class IntervalService {
-  interval!: ReturnType<typeof setInterval>;
+  private interval!: ReturnType<typeof setInterval>;
 
-  setInterval(time: number, callback: () => void) {
+  setInterval(time: number, callback: () => void): void {
     this.interval = setInterval(callback, time);
   }
 
-  clearInterval() {
+  clearInterval(): void {
     clearInterval(this.interval);
   }
 }
