12115abed56712d68705fd3a758b4a4c649e99e3
Ziomioslaw <dawid.plocki@gmail.com>
2023-07-10 21:23:01 +0200

    Setup tests fro LocalStorageService

--- src/app/services/local-storage.service.spec.ts
+++ src/app/services/local-storage.service.spec.ts
@@ -1,16 +1,26 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LocalStorageService } from './local-storage.service';
+import { LoggerService } from './logger.service';
 
 describe('LocalStorageService', () => {
-  let service: LocalStorageService;
+  let localStorageService: LocalStorageService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(LocalStorageService);
+    TestBed.configureTestingModule({
+      providers: [
+        LocalStorageService,
+        {
+          provide: LoggerService,
+          useValue: jasmine.createSpyObj('LocalStorageService', ['log'])
+        }
+      ]
+    });
+
+    localStorageService = TestBed.inject(LocalStorageService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(localStorageService).toBeTruthy();
   });
 });
