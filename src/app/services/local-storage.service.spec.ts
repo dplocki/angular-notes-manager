21a88fcd97c9fa78a8537b43450f27b0c37148d1
Ziomioslaw <dawid.plocki@gmail.com>
2023-07-12 20:23:05 +0200

    Add unit tests for LocalStorage

--- src/app/services/local-storage.service.spec.ts
+++ src/app/services/local-storage.service.spec.ts
@@ -2,9 +2,12 @@ import { TestBed } from '@angular/core/testing';
 
 import { LocalStorageService } from './local-storage.service';
 import { LoggerService } from './logger.service';
+import { Note } from '../note';
 
 describe('LocalStorageService', () => {
   let localStorageService: LocalStorageService;
+  let loggerServiceSpy: jasmine.SpyObj<LoggerService>;
+  let localStoreSpy: jasmine.SpyObj<Storage>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -12,15 +15,41 @@ describe('LocalStorageService', () => {
         LocalStorageService,
         {
           provide: LoggerService,
-          useValue: jasmine.createSpyObj('LocalStorageService', ['log'])
+          useValue: jasmine.createSpyObj('LoggerService', ['log'])
         }
       ]
     });
 
     localStorageService = TestBed.inject(LocalStorageService);
+    loggerServiceSpy = TestBed.inject(LoggerService) as jasmine.SpyObj<LoggerService>;
+  });
+
+  beforeEach(() => {
+    let fakeLocalStore: { [id: string]: string | null } = {};
+
+    localStoreSpy = {
+      getItem: spyOn(window.localStorage, 'getItem').and.callFake((key: string) => key in fakeLocalStore ? fakeLocalStore[key] : null),
+      setItem: spyOn(window.localStorage, 'setItem').and.callFake((key, value) => (fakeLocalStore[key] = value + '')),
+      clear: spyOn(window.localStorage, 'clear').and.callFake(() => (fakeLocalStore = {}))
+    } as jasmine.SpyObj<Storage>;
   });
 
   it('should be created', () => {
     expect(localStorageService).toBeTruthy();
+  });
+
+  it('saveNotes is calling the logger and the save the local storage', async () => {
+    const notes: Note[] = [
+      new Note('abc', 1),
+      new Note('bcd', 2)
+    ];
+
+    const results = await localStorageService.saveNotes(notes);
+
+    expect(results).toEqual(notes);
+    expect(loggerServiceSpy.log).toHaveBeenCalled();
+    expect(localStoreSpy.setItem).toHaveBeenCalled();
+    expect(localStoreSpy.getItem).not.toHaveBeenCalled();
+    expect(localStoreSpy.clear).not.toHaveBeenCalled();
   });
 });
