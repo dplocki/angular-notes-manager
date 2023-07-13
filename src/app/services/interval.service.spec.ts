9aca06d271cc2e74f237aa6cd1308a94b8bee241
Ziomioslaw <dawid.plocki@gmail.com>
2023-07-13 19:52:14 +0200

    Add unit tests for IntervalService

--- src/app/services/interval.service.spec.ts
+++ src/app/services/interval.service.spec.ts
@@ -3,14 +3,40 @@ import { TestBed } from '@angular/core/testing';
 import { IntervalService } from './interval.service';
 
 describe('IntervalService', () => {
-  let service: IntervalService;
+  let intervalService: IntervalService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(IntervalService);
+    intervalService = TestBed.inject(IntervalService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(intervalService).toBeTruthy();
+  });
+
+  it('setInterval should call the setInterval with given paramaters', () => {
+    const givenCallback = (): void => {};
+    const givenTime = 1002;
+    const spySetInterval = spyOn(window, 'setInterval').and.returnValue(123 as unknown as ReturnType<typeof setInterval>);
+
+    intervalService.setInterval(givenTime, givenCallback);
+
+    expect(spySetInterval).toHaveBeenCalled();
+    expect(spySetInterval).toHaveBeenCalledWith(givenCallback, givenTime);
+  });
+
+  it('clearInterval should call the clearInterval with paramater recieved from setInterval', () => {
+    const givenCallback = (): void => {};
+    const givenTime = 1002;
+    const intervalTimerValue = 123;
+    const spySetInterval = spyOn(window, 'setInterval').and.returnValue(intervalTimerValue as unknown as ReturnType<typeof setInterval>);
+    const spyClearInterval = spyOn(window, 'clearInterval');
+
+    intervalService.setInterval(givenTime, givenCallback);
+    intervalService.clearInterval();
+
+    expect(spySetInterval).toHaveBeenCalled();
+    expect(spySetInterval).toHaveBeenCalledWith(givenCallback, givenTime);
+    expect(spyClearInterval).toHaveBeenCalledWith(intervalTimerValue);
   });
 });
