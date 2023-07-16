01c88cb0e36c5d82be76359797dee9bf51474962
Ziomioslaw <dawid.plocki@gmail.com>
2023-07-16 18:39:33 +0200

    Rearange the unit tests for NoteTitlePipe

--- src/app/note-title.pipe.spec.ts
+++ src/app/note-title.pipe.spec.ts
@@ -7,18 +7,21 @@ describe('NoteTitlePipe', () => {
     expect(pipe).toBeTruthy();
   });
 
-  function callTransform(inMessage: string, expectedMessage: string) {
-    return () => {
-      const pipe = new NoteTitlePipe();
-      const result = pipe.transform(new Note(inMessage, 12));
+  describe('transform', () => {
 
-      expect(result).toBe(expectedMessage);
+    function callTransform(inMessage: string, expectedMessage: string) {
+      return () => {
+        const pipe = new NoteTitlePipe();
+        const result = pipe.transform(new Note(inMessage, 12));
+
+        expect(result).toBe(expectedMessage);
+      }
     }
-  }
 
-  it('transform for not undefined should return empty string', callTransform(undefined as unknown as string, ''));
-  it('transform for white characters should return empty string', callTransform('                \n            ', ''));
-  it('transform for simple case should return the text of the note', callTransform('aasaasasasasa', 'aasaasasasasa'));
-  it('transform for too long text return the trimed', callTransform('123456789012345678901234567890', '12345678901234567890...'));
-  it('transform for new line should be removed before ', callTransform('aasaa\nsasasasa', 'aasaasasasasa'));
+    it('for not undefined should return empty string', callTransform(undefined as unknown as string, ''));
+    it('for white characters should return empty string', callTransform('                \n            ', ''));
+    it('for simple case should return the text of the note', callTransform('aasaasasasasa', 'aasaasasasasa'));
+    it('for too long text return the trimed', callTransform('123456789012345678901234567890', '12345678901234567890...'));
+    it('for new line should be removed before ', callTransform('aasaa\nsasasasa', 'aasaasasasasa'));
+  });
 });
