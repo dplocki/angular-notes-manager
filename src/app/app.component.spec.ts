da93bfcb5c7914240b8d55df094fbc2a55453bc6
Ziomioslaw <dawid.plocki@gmail.com>
2023-07-25 19:25:34 +0200

    Set up of NoteService mock for AppComponent unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,10 +3,15 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { Component, EventEmitter, Input, Output, WritableSignal } from '@angular/core';
 import { Note } from './note';
+import { NoteService } from './services/note.service';
+import { IntervalService } from './services/interval.service';
+import { BrowserInteractionService } from './services/browser-interaction.service';
+import { makeNote, multiple } from './shared/testing/generators';
 
 describe('AppComponent', () => {
   let appComponent: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
+  let noteServiceSpy: jasmine.SpyObj<NoteService>;
 
   @Component({ selector: 'app-note-detail', template: '' })
   class MockNoteDetailComponent {
@@ -47,8 +52,26 @@ describe('AppComponent', () => {
         MockNewButtonComponent,
         MockSaveButtonComponent
       ],
-      imports: [FormsModule]
+      imports: [FormsModule],
+      providers: [
+        {
+          provide: NoteService,
+          useValue: jasmine.createSpyObj('NoteService', ['getNotes', 'createNote', 'deleteNote', 'saveNote'])
+        },
+        {
+          provide: IntervalService,
+          useValue: jasmine.createSpyObj('IntervalService', ['setInterval', 'clearInterval'])
+        },
+        {
+          provide: BrowserInteractionService,
+          useValue: jasmine.createSpyObj('BrowserInteractionService', ['question'])
+        },
+      ]
     });
+
+    noteServiceSpy = TestBed.inject(NoteService) as jasmine.SpyObj<NoteService>;
+    noteServiceSpy.createNote.and.returnValue(makeNote().setText('').make());
+    noteServiceSpy.getNotes.and.resolveTo([]);
 
     fixture = TestBed.createComponent(AppComponent);
     appComponent = fixture.componentInstance;
@@ -56,6 +79,8 @@ describe('AppComponent', () => {
   });
 
   it('should create the app', () => {
+
+    expect(noteServiceSpy.createNote).toHaveBeenCalled();
     expect(appComponent).toBeTruthy();
   });
 
