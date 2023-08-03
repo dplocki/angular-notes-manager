7f8de112dac3542150343f17e11148641c1cee46
Ziomioslaw <dawid.plocki@gmail.com>
2023-08-03 19:18:21 +0200

    Add unit tests for NoteTitleComponent

--- src/app/note-title/note-title.component.spec.ts
+++ src/app/note-title/note-title.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NoteTitleComponent } from './note-title.component';
 import { NoteTitlePipe } from '../note-title.pipe';
+import { By } from '@angular/platform-browser';
+import { newString } from '../shared/testing/generators';
 
 describe('NoteTitleComponent', () => {
   let component: NoteTitleComponent;
@@ -18,5 +20,26 @@ describe('NoteTitleComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show empty label, if not giving title', () => {
+    component.title = undefined;
+    const linksWithEmptyClasses = fixture.debugElement.queryAll(By.css('a.empty'));
+
+    expect(linksWithEmptyClasses.length).toBe(1);
+  });
+
+  it('should show empty label, if giving empty title', () => {
+    component.title = '';
+    const linksWithEmptyClasses = fixture.debugElement.queryAll(By.css('a.empty'));
+
+    expect(linksWithEmptyClasses.length).toBe(1);
+  });
+
+  it('should show title, when it is not empty', () => {
+    component.title = newString();
+    const linksWithEmptyClasses = fixture.debugElement.queryAll(By.css('a.empty'));
+
+    expect(linksWithEmptyClasses.length).toBe(1);
   });
 });
